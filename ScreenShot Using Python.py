6a28f412f47cba9e3b072a76684d39df443e7476
Mr Programmer <88921795+TanmayProgrammer-13@users.noreply.github.com>
2021-10-27 04:14:07 -0700

    Take Screen Shot Using Python
    Take Screen Shot Using Python

---
+++ ScreenShot Using Python.py
@@ -0,0 +1,14 @@
+#Take ScreenShot Using Python
+import pyscreenshot
+
+import pyscreenshot
+
+#Capture Full Screen 
+image = pyscreenshot.grab()
+
+#Display The Captured ScreenShot
+image.show()
+
+#Save the Captured ScreenShot
+image.save("PyScreen1")
+
