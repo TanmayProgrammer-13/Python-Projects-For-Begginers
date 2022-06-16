931cbfd4d584e150abcb9a9c137d73802a7a429f
Mr Programmer <88921795+TanmayProgrammer-13@users.noreply.github.com>
2022-06-16 23:00:03 +0530

    Web Server Python
    Web Server

---
+++ Web-Server/README.md
@@ -0,0 +1,16 @@
+# Web Server - Python 3
+
+## What is a Web Server?
+A web server is computer software and underlying hardware that accepts requests via HTTP or its secure variant HTTPS. 
+
+## Prerequests for Server
+
+- HTTPServer
+- BaseHTTPRequestHandler
+
+## Aurthor
+- [@TanmayProgrammer-13](https://github.com/TanmayProgrammer-13)
+
+## Languages Used 
+- Python 3
+
---
+++ Web-Server/server.py
@@ -0,0 +1,29 @@
+# Basic Web Server In Python
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 3000
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Web Server - Python</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is a Web Server Hosted on a Local System</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
